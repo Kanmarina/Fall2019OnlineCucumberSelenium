005d5ae4cebc6fac6d38660d2e5e2f893b80b74d
Kanmarina <kanmarina.coach@yahoo.com>
2020-04-19 17:14:38 -0500

    first commit

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,23 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/vytrack/step_definitions",
+        features = "src/test/resources/features",
+        dryRun = false,
+        strict = false,
+        tags = "@create_car",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json"
+        }
+
+)
+public class CucumberRunner {
+
+
+}
